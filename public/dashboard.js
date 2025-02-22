04290e947806db67e14d0b933cd323b787fd74d9
yusufaakarsu <yusufaakarsu@gmail.com>
2025-02-23 01:08:33 +0300

    side bar9

--- public/dashboard.js
+++ public/dashboard.js
@@ -2,10 +2,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
     // Header'ı yükle
     await loadSideBar();
-    
-    // Header yüklendikten sonra içeriği doğru konumlandır
-    document.querySelector('.ms-lg-200').style.marginLeft = '200px';
-    
     await loadDashboardData();
     await loadRecentOrders();
 });
